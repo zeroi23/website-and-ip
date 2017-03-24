93c57ce22d5c6a881c1cd12abef4406de43a63c7
zeroi23 <zongchaomail@gmail.com>
2017-03-24 18:48:46 +0800

    优化有时会漏掉最初的一些请求,增加显示和隐藏,增加全局存储显示变量

--- background.js
+++ background.js
@@ -1,7 +1,19 @@
-chrome.webRequest.onCompleted.addListener(function(detail) {
-	// console.debug(detail);
-	chrome.tabs.sendMessage(detail.tabId, detail);
-	return;
+chrome.webRequest.onCompleted.addListener(function (detail) {
+    // console.debug(detail);
+    var tabId = detail.tabId;
+    if (tabId != -1) {
+        chrome.tabs.get(tabId, function (tab) {
+            // console.debug(tab.status);
+            if (tab.status == "loading") {
+                //console.debug(tabId+" is loading");
+                setTimeout(function () {
+                    chrome.tabs.sendMessage(tabId, detail)
+                }, 100);
+            } else {
+                chrome.tabs.sendMessage(tabId, detail)
+            }
+        })
+    }
 }, {
-	urls : []
+    urls: []
 });
--- ip.js
+++ ip.js
@@ -1,54 +1,79 @@
 var domainList = {};
+var key = "show";
+var isShow = true;
+{
+    chrome.storage.local.get(key, function (item) {
+        isShow = item[key];
+        console.debug(isShow);
+    });
+}
+chrome.runtime.onMessage.addListener(function (detail) {
+        // console.debug(detail);
+        if (detail.url != undefined) {
+            var start = detail.url.indexOf("//");
+            var end = detail.url.indexOf("/", start + 2);
+            var url = detail.url.substring(start + 2, end);
+            if (domainList[url] == undefined) {
+                var domain = {};
+                domain.url = url;
+                domain.ip = detail.ip;
+                domain.count = 1;
+                domain.urls = [detail.url];
+                domainList[url] = domain;
+            } else {
+                var domain = domainList[url];
+                domain.urls.push(detail.url);
+                ++domain.count;
+            }
+            if (isShow && $("#chrome-IP").length == 1) {
+                renderHtml();
+            }
+        }
+    }
+);
 
-chrome.runtime.onMessage.addListener(function(detail) {
-//	console.debug(detail.url);
-	if (detail.url != undefined) {
-		var start = detail.url.indexOf("//");
-		var end = detail.url.indexOf("/", start + 2);
-		var url = detail.url.substring(start + 2, end);
-		if (domainList[url] == undefined) {
-			var domain = new Object();
-			domain.url=url;
-			domain.ip = detail.ip;
-			domain.count = 1;
-			domain.urls=[detail.url];
-			domainList[url] = domain;
-		} else {
-			var domain=domainList[url];
-			domain.urls.push(detail.url);
-			++domain.count;
-		}
-		if ($("#chrome-IP").length == 1) {
-			var display = "";
-			for ( var o in domainList) {
-				var domain = domainList[o];
-				display += getDomainHtml(domain);
-			}
-			$("#chrome-IP .ipList").html(display);
-		}
-	}
-});
-
-function getDomainHtml(domain) {
-	var title="";
-	for(var i in domain.urls){
-		title+=domain.urls[i]+"\n";
-	}
-	return '<tr><td class="ip" title=\''+title+'\'><a target="_blank" href="http://'
-			+ domain.url
-			+ '">'
-			+ domain.url
-			+ '</a></td><td><a target="_blank" href="http://wap.ip138.com/ip_search138.asp?ip='
-			+ domain.ip + '">' + domain.ip + '</td><td>' + domain.count
-			+ '</td></tr>';
+function renderHtml() {
+    var display = "";
+    for (var o in domainList) {
+        var domain = domainList[o];
+        display += getDomainHtml(domain);
+    }
+    $("#chrome-IP .ipList").html(display);
 }
 
-$(function() {
-	$("body")
-			.append(
-					'<div id="chrome-IP" class="chrome_right"><table class="ipList"></table><span class="close">关闭</span></div>');
+function getDomainHtml(domain) {
+    var title = "";
+    for (var i in domain.urls) {
+        title += domain.urls[i] + "\n";
+    }
+    return '<tr><td class="ip" title=\'' + title + '\'><a target="_blank" href="http://'
+        + domain.url
+        + '">'
+        + domain.url
+        + '</a></td><td><a target="_blank" title="点击查询IP' + domain.ip + '" href="http://wap.ip138.com/ip_search138.asp?ip='
+        + domain.ip + '">' + domain.ip + '</td><td>' + domain.count
+        + '</td></tr>';
+}
 
-	$(".close", "#chrome-IP").click(function() {
-		$("#chrome-IP").hide();
-	});
+$(function () {
+    $("body")
+        .append(
+            '<div id="chrome-IP" class="chrome_right">' +
+            '<table class="ipList"></table>' +
+            '<div class="float-right"><span id="show">显示</span><span' +
+            ' id="hidden">隐藏</span><span' +
+            ' class="close">关闭</span></div></div>');
+
+    $(".close", "#chrome-IP").click(function () {
+        $("#chrome-IP").hide()
+    });
+    $("#show", "#chrome-IP").click(function () {
+        $("#chrome-IP .ipList").show();
+        renderHtml();
+        chrome.storage.local.set({"show": true});
+    });
+    $("#hidden", "#chrome-IP").click(function () {
+        $("#chrome-IP .ipList").hide();
+        chrome.storage.local.set({"show": false});
+    });
 });
